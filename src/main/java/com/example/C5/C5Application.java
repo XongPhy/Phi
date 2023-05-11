4ce92ebd42d4e0876bc575d98c72e29d6b498aeb
2011062999_DuongNguyenSongPhi <songphi02012511@gmail.com>
2023-05-11 13:04:41 +0700

    First Commit

---
+++ src/main/java/com/example/C5/C5Application.java
@@ -0,0 +1,13 @@
+package com.example.C5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/C5/C5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.C5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
